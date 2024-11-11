9a488a112592b48adc9e32d69344c950ec1cbac4
Md Aljubayer <tanzimnahid6@gmail.com>
2024-11-11 20:02:18 +0600

    test

--- app.js
+++ app.js
@@ -12,14 +12,19 @@ connectDB();
 const app = express();
 
 // Enable CORS with default settings (allows all origins)
-app.use(cors());
+const corsOptions = {
+    origin: 'http://localhost:5173', // Replace with your frontend's domain
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  };
 
 // Optionally, customize CORS settings
-app.use(cors({
-  origin: 'http://localhost:5173', // Allow only specific origin
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'] // Restrict to specific HTTP methods if needed
-}));
+// app.use(cors({
+//   origin: 'http://localhost:5173', // Allow only specific origin
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//   allowedHeaders: ['Content-Type', 'Authorization'] // Restrict to specific HTTP methods if needed
+// }));
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
